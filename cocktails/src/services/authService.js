6cfb120f7e3f8a0b54d0b4aea59c39cb58f93a5f
Simona <simonapalazova@gmail.com>
2023-12-07 19:41:34 +0200

    create authServices

---
+++ cocktails/src/services/authService.js
@@ -0,0 +1,31 @@
+import * as request from '../lib/request';
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const login = async (email, password) => {
+    const result = await request.post(`${baseUrl}/login`, {
+        email,
+        password,
+    });
+
+    return result;
+};
+
+export const register = (username, email, password, rePassword) => request.post(`${baseUrl}/register`, {
+    username,
+    email,
+    password,
+    rePassword
+});
+
+export const logout = () => request.get(`${baseUrl}/logout`);
+
+export const getProfile = (userId) => request.get(`${baseUrl}/profile`, {
+    userId
+});
+
+export const editProfile = (userId, username, email) => request.post(`${baseUrl}/profile/edit`, {
+    userId,
+    username, 
+    email
+});
