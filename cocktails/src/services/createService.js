1b4f96504f2ba5f2c81799dd16b92fd27e69e83b
Simona <simonapalazova@gmail.com>
2023-12-07 19:49:36 +0200

    modify

--- cocktails/src/services/createService.js
+++ cocktails/src/services/createService.js
@@ -2,9 +2,10 @@ import * as request from '../lib/request';
 
 const baseUrl = 'http://localhost:3030/create';
 
-export const register = (name, image, ingredients, methodOfPreparation ) => request.post(`${baseUrl}/register`, {
+export const createCocktail = (name, image, ingredients, methodOfPreparation, userId ) => request.post(`${baseUrl}/register`, {
     name, 
     image, 
     ingredients, 
-    methodOfPreparation 
+    methodOfPreparation,
+    userId
 });
