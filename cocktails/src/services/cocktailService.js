f1a0d1fa2f486d8bc4f35d2013d26e3c43ceb569
Simona <simonapalazova@gmail.com>
2023-12-08 11:14:25 +0200

    Create cocktailService.js

---
+++ cocktails/src/services/cocktailService.js
@@ -0,0 +1,23 @@
+import * as request from '../lib/request';
+
+const baseUrl = 'http://localhost:3030/cocktails';
+
+export const getAllCocktail = () => request.get(`${baseUrl}`);
+
+export const getOneCocktail = (cocktailId) => request.get(`${baseUrl}/${cocktailId}`,{
+    cocktailId
+});
+
+export const editCocktail = (cocktailId, name, image, ingredients, methodOfPreparation) => 
+request.put(`${baseUrl}/${cocktailId}/edit`, {
+    name, 
+    image, 
+    ingredients, 
+    methodOfPreparation
+});
+
+export const deleteCocktail = (cocktailId, userId) => request.remove(`${baseUrl}/${cocktailId}/delete`, {
+    userId
+});
+
+export const getCocktailsByOwner = (userId) => request.get(`${baseUrl}/${userId}/cocktails`);
