9a7d30d024c8a415d435be524bd4f36d7d722643
Simona <simonapalazova@gmail.com>
2023-12-08 11:37:34 +0200

    Create paths.js

---
+++ cocktails/src/paths.js
@@ -0,0 +1,15 @@
+const Path = {
+    Home: '/',
+    Logout: '/logout',
+    Login: '/login',
+    Register: '/register',
+    CreateCocktail: '/create/cocktail',
+    Cocktails: '/cocktails',
+    CocktailEdit: '/cocktails/:cocktailId/edit',
+    CocktailDelete: '/cocktails/:cocktailId/delete',
+    CocktailDetails: '/cocktails/:cocktailId',
+    Profile: '/profile',
+    EditProfile: '/edit/profile',
+};
+
+export default Path;
